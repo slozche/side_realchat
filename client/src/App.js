1405752a30fb647586e5eae6b1864798446cdda2
slozche <ldhfj2141@gmail.com>
2022-11-03 16:26:29 +0900

    ✨ 클라이언트 연결

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div>리액트로 만든 HTML 이다.</div>
+    </div>
+  );
+}
+
+export default App;
--- server.js
+++ server.js
@@ -5,3 +5,9 @@ const app = express();
 app.listen(8080, function () {
   console.log("listening on 8080");
 });
+
+app.use(express.static(path.join(__dirname, "./client/build")));
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
